8e03fb9e9a7bab3cd8729e0d2dba76aba0dccb4b
mharoonmugal-web <mharoonmugal@gmail.com>
2026-06-10 18:01:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Alpha Finance - Eligibility Engine", layout="wide")
+st.set_page_config(page_title="Digital Credit Engine", layout="wide")
 
 # -----------------------------
 # CONFIG
